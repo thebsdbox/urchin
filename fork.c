1d8a40e07ee16abf23282675a8fd0371960a26a4
thebsdbox <daniel.finneran@gmail.com>
2018-04-17 17:20:56 +0100

    updated forking

--- fork.c
+++ fork.c
@@ -2,21 +2,41 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <signal.h>
 
 int forkSelf (int zombieCount)
 {
     pid_t child_pid;
     for (int i = 0; i < zombieCount; i++) {
         
-        child_pid = fork ();
+        child_pid = fork();
         if (child_pid > 0) {
             // parent process will sleep for 30 seconds and exit, without a call to wait()
             fprintf(stderr,"parent process - %d\n", getpid());
-            sleep(1);
+            //sleep(1);
+            //exit(0);
         }
         else if (child_pid == 0) {
             // Child process is killed immediately, but the parent never cleans up! ZOMBIE TIME
             fprintf(stderr,"child process - %d\n", getpid());
+            signal(SIGHUP, SIG_IGN);
+            close(0);
+            close(1);
+            close(2);
+            //
+            chdir("/");
+            //
+            //
+            setsid();
+            //
+            //while(1) {
+            //    sleep(1);
+            //}
+            
+            //            setsid();
+            //
             exit(0);
         }
         else if (child_pid == -1) {
@@ -32,3 +52,27 @@ int forkSelf (int zombieCount)
     }
     return 0;
 }
+
+int becomeParent() {
+
+        pid_t pid;
+        
+        if (( pid = fork()) < 0) exit(0);
+        else if(pid != 0) exit(0); // kill parent process
+        
+        fprintf(stderr,"child running...pid=%d\n",getpid());
+        signal(SIGHUP, SIG_IGN);
+        close(0);
+        close(1);
+        close(2);
+        //
+        chdir("/");
+        //
+        //
+        setsid();
+        //
+        while(1) {
+            sleep(1);
+        }
+    }
+
