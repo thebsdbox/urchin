76384973f4eec2a67b28c458e86d6b518f619321
Dan Finneran <daniel.finneran@gmail.com>
2018-05-11 13:39:08 +0100

    Local image building

--- make_local_image.sh
+++ make_local_image.sh
@@ -4,10 +4,11 @@ echo ""
 echo "Dynamically building UI from ./htmlfiles"
 xxd -i htmlfiles/index.html >> httpui.h
 echo "Moving files to the dockerfile directory"
-cp *.c ./local_dockerfile
-cp *.h ./local_dockerfile
+mkdir ./local_dockerfile/tar
+find . -name "*.c" -o -name "*.h" | tar -cf ./local_dockerfile/tar/urchin.tar -T -
 cd ./local_dockerfile
 docker build -t urchin:local .
 echo "Restoring local_dockerfile directory and include files"
-git checkout -- ./local_dockerfile
+rm -rf ./local_dockerfile/tar
+cd ..
 git checkout -- ./httpui.h
